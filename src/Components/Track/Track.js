cbeaf1062ccaaddf9f1d6ec9ab6aa581f7ea147c
browe83 <rowe.brandon.j@gmail.com>
2020-09-29 16:19:02 -0400

    updates Playlist, Track

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -4,11 +4,11 @@ import "./Track.css";
 class Track extends Component {
   constructor(props) {
     super(props);
-    this.isRemoval = false;
+
     this.renderAction = this.renderAction.bind(this);
   }
   renderAction() {
-    return this.isRemoval ? (
+    return this.props.isRemoval ? (
       <button className="Track-action">-</button>
     ) : (
       <button className="Track-action">+</button>
@@ -21,7 +21,7 @@ class Track extends Component {
           {/* <h3><!-- track name will go here --></h3>
                     <p><!-- track artist will go here--> | <!-- track album will go here --></p> */}
         </div>
-        {/* <button className="Track-action"><!-- + or - will go here --></button> */}
+        {this.renderAction()}
       </div>
     );
   }
