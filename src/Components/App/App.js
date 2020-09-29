c8dc3dd83268a514134270fdde39c764edc3660f
browe83 <rowe.brandon.j@gmail.com>
2020-09-29 15:57:23 -0400

    fixes css

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,24 +1,26 @@
 import React from "react";
 import "./App.css";
 import SearchBar from "../SearchBar/SeachBar";
-import SearchResults from "..SearchResults/SearchResults/SearchResults";
-import Playlist from "..Playlist/Playlist/Playlist";
+import SearchResults from "../SearchResults/SearchResults";
+import Playlist from "../Playlist/Playlist";
 
-function App() {
-  return (
-    <div>
-      <h1>
-        Ja<span className="highlight">mmm</span>ing
-      </h1>
-      <div className="App">
-        <SearchBar />
-        <div className="App-playlist">
-          <SearchResults />
-          <Playlist />
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Ja<span className="highlight">mmm</span>ing
+        </h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults />
+            <Playlist />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
