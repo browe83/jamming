b73527e89636443df369b6e97aba8f9894ee83c0
browe83 <rowe.brandon.j@gmail.com>
2020-10-01 14:11:44 -0400

    removes tracks from playlist

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -7,10 +7,13 @@ class Track extends Component {
 
     this.renderAction = this.renderAction.bind(this);
     this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
   }
   renderAction() {
     return this.props.isRemoval ? (
-      <button className="Track-action">-</button>
+      <button className="Track-action" onClick={this.removeTrack}>
+        -
+      </button>
     ) : (
       <button className="Track-action" onClick={this.addTrack}>
         +
@@ -19,6 +22,9 @@ class Track extends Component {
   }
   addTrack() {
     this.props.onAdd(this.props.track);
+  }
+  removeTrack() {
+    this.props.onRemove(this.props.track);
   }
   render() {
     return (
