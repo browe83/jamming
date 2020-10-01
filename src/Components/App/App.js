e8c3ccd93f8cb39709b44f60d50a3f915e285681
browe83 <rowe.brandon.j@gmail.com>
2020-10-01 14:52:17 -0400

    changes name of a playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -37,6 +37,7 @@ class App extends React.Component {
     };
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
   addTrack(track) {
     let tracks = this.state.playlistTracks;
@@ -57,6 +58,11 @@ class App extends React.Component {
       playlistTracks: tracks,
     });
   }
+  updatePlaylistName(name) {
+    this.setState({
+      playlistName: name,
+    });
+  }
   render() {
     return (
       <div>
@@ -74,6 +80,7 @@ class App extends React.Component {
               playlistName={this.state.playlistName}
               playlistTracks={this.state.playlistTracks}
               onRemove={this.removeTrack}
+              onNameChange={this.updatePlaylistName}
             />
           </div>
         </div>
